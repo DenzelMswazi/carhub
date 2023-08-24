5dcdd812f8a532052b39801dd493587dc29626b3
DenzelMswazi <denzelmwangi94@gmail.com>
2023-08-24 10:50:03 +0300

    Added-copyright-content

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,7 +17,7 @@ const Footer = () => {
           />
           <p className="text-base text-gray-700">
             Carhub 2023 <br />
-            All rights reserved &copy;
+            Created by Denzel-Mswazi All rights reserved &copy;
           </p>
         </div>
 
@@ -36,6 +36,18 @@ const Footer = () => {
               ))}
             </div>
           ))}
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center flex-wrap mt-10 border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2023 CarHub.All rights reserved</p>
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
         </div>
       </div>
     </footer>
