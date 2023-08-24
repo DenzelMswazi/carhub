60830edd240ac5a3d59cb72e15dd165589e14860
DenzelMswazi <denzelmwangi94@gmail.com>
2023-08-24 12:20:36 +0300

    Added-car-catalogue-link

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import Image from 'next/image'
-import { Hero } from '@/components'
+import Image from "next/image";
+import { Hero } from "@/components";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-     <Hero />
+      <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the Cars you might like</p>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
