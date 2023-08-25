0982a7491f081d79bb312348f2dc6905629cd1c6
DenzelMswazi <denzelmwangi94@gmail.com>
2023-08-25 14:47:53 +0300

    Imported-headless-ui-to-the components

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { Combobox, Transition } from "@headlessui/react";
+import { SearchManufacturerProps } from "@/types";
 import React from "react";
 
-const SearchManufacturer = () => {
-  return <div></div>;
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) => {
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full"></div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
