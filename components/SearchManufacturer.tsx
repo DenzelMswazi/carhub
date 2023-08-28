97536f2b3bc52ae618a9755851f613003787ee23
DenzelMswazi <denzelmwangi94@gmail.com>
2023-08-28 13:15:38 +0300

    Added-Combobox-button

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,16 +1,38 @@
 "use client";
 import { Combobox, Transition } from "@headlessui/react";
 import { SearchManufacturerProps } from "@/types";
-import React from "react";
+import Image from 'next/image';
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+
+  const filteredManufacturers = 
+  query === ""
+  ? manufacturers
+  : manufacturers.filter((item) =>
+    item
+    .toLowerCase()
+    .replace(/\s+/g, "")
+    .includes(query.toLowerCase().replace(/\s+/g,""))
+    );
+
   return (
     <div className="search-manufacturer">
       <Combobox>
-        <div className="relative w-full"></div>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+            src="/car-logo.svg" 
+            width={20}
+            height={20}
+            className="ml-4"
+            alt="Car Logo"
+            />
+          </Combobox.Button>
+        </div>
       </Combobox>
     </div>
   );
