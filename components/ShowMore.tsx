0ef38e365f94e830b087b079133511e65e99e9f5
DenzelMswazi <denzelmwangi94@gmail.com>
2023-09-01 09:23:47 +0300

    Created-showMore-button

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -18,4 +18,18 @@ const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
 
     router.push(newpathname);
   };
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          btnType="button"
+          title="Show More"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
 };
+export default ShowMore;
