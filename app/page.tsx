8133258ccd49abd0502f4619373357d201ca8752
DenzelMswazi <denzelmwangi94@gmail.com>
2023-09-01 13:47:45 +0300

    Resolved-function-to-check-if-the-car-is-available

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,15 @@ import { fuels, yearsOfProduction } from "@constants";
 import { CustomFilter, Hero, SearchBar, ShowMore, CarCard } from "@/components";
 
 export default async function Home({ searchParams }: HomeProps) {
-        const allCars = await fetchCars({
-          manufacturer: searchParams.manufacturer || "",
-          year: searchParams.year || 2022,
-          fuel: searchParams.fuel || "",
-          limit: searchParams.limit || 10,
-          model: searchParams.model || "",
-        });
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
-        const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
-
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
     <main className="overflow-hidden">
@@ -30,25 +29,28 @@ export default async function Home({ searchParams }: HomeProps) {
 
           <div className="home__filer-container">
             <CustomFilter title="fuel" options={fuels} />
-            <CustomFilter title="year" options={yearsOfProduction}/>
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
         {!isDataEmptympty ? (
           <section>
             <div className="home__cars-wrapper">
-               {allCars?.map((car) => (
+              {allCars?.map((car) => (
                 <CarCard car={car} />
-               ))}
+              ))}
             </div>
 
-            <ShowMore 
-            pageNumber={(searchParams.limit || 10) / 10}
-            isNext={(searchParams.limit || 10) > allCars.length}
-             />
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
-        ): (
-            
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no result</h2>
+            <p>{allCars?.message}</p>
+          </div>
         )}
       </div>
     </main>
