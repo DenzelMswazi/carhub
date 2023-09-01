a0fb6d2652d9a6de0943f6135bcec75cff24b5fa
DenzelMswazi <denzelmwangi94@gmail.com>
2023-09-01 12:05:37 +0300

    Function-to-search-car-parameters

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,18 @@ import { HomeProps } from "@types";
 import { fuels, yearsOfProduction } from "@constants";
 import { CustomFilter, Hero, SearchBar, ShowMore, CarCard } from "@/components";
 
-export default function Home() {
+export default async function Home({ searchParams }: HomeProps) {
+        const allCars = await fetchCars({
+          manufacturer: searchParams.manufacturer || "",
+          year: searchParams.year || 2022,
+          fuel: searchParams.fuel || "",
+          limit: searchParams.limit || 10,
+          model: searchParams.model || "",
+        });
+
+        const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+        
   return (
     <main className="overflow-hidden">
       <Hero />
