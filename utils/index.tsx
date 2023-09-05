0dce03ffa845ccc813487d6046bf8cf40ed5f106
DenzelMswazi <denzelmwangi94@gmail.com>
2023-09-05 14:31:26 +0300

    Async-function-to-fetch-cars

--- utils/index.tsx
+++ utils/index.tsx
@@ -39,3 +39,24 @@ export const updateSearchParams = (type: string, value: string) => {
 
     return newPathname;
  };
+ 
+ export async function fetchCars(filters: FilterProps) {
+    const { manufacturer, year, model, limit, fuel } = filters;
+
+    // Set the required headers for the API request
+    const headers : HeadersInit = {
+        "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+        "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapirapi.com",
+    };
+    //Set the required headers for the API request 
+    const response = await fetch(
+        `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+        {
+            headers: headers,
+        } 
+    );
+    //Parse the response as JSON
+   const result = await response.json();
+
+   return result;
+ }
